efd84cc2d97e41c466dc4e07d726d6793579a319
venkateshnakka96 <nakkavenkatesh967@gmail.com>
2025-02-05 08:36:12 +0530

    Add files via upload

---
+++ day1.py/1.py
@@ -0,0 +1,32 @@
+
+#num1=23456789
+####print(num1*num2)
+print(num1/num2)
+print(num1//num2)
+
+
+#print('hi_i _am_ venkatesh...')
+venkatesh_nakka=('i _am_venkatesh')
+print(venkatesh_nakka)
+
+
+#str1='jai mahishmathi'
+print(len(str1))
+print(str1[5])
+print(str1[14])
+print(str1[-6])
+
+print(str1[0:8])
+print(str1[6:])
+print(str1[:13])
+print(str1[:])
+print(str1[0:1000])
+print(str1[-16:0])
+print(str1[0:6:15])
+print(str1[0:15])
+print(str1[0: 7: 15])
+
+
+
+
+print(23456789+25789999)
---
+++ day1.py/day2.py
@@ -0,0 +1,4 @@
+num1=23456789
+num2=25789999
+print('substraction')
+print(num1+num2)
